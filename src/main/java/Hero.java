ad4d6de939e2072b759d72ce0c169071dd7803ae
felixmaina <karani.maina2010@gmail.com>
2022-01-28 15:24:27 +0300

    create hero test

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,13 @@
 public class Hero {
+private String name ;
+private int age;
+private String specialPower;
+private String weakness;
 
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
 }
